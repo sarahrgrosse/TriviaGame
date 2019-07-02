dba78c869cf60278f9449eaf44f010158899e681
Sarah Rhianna Grosse <sarahrgrosse@Sarahs-MacBook-Air.local>
2019-07-02 15:39:40 -0400

    added questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,23 @@
-console.log("Hello World");
+$(document).ready(function () {
+    $("#quiz").submit(function (event) {
+        event.preventDefault();
 
-function main(){
-    console.log("World Hello")
-}
-main()
+        var $inputs = $('#quiz input[type=\'radio\']:checked');
+
+        let correctAnswers = 0
+
+        $inputs.each(function () {
+            // if the value is true, they chose the correct answer
+            let answer = $(this).val();
+            if (answer === "true") {
+                correctAnswers = correctAnswers + 1
+            }
+
+
+
+        })
+
+        console.log(`you have this many correct: ${correctAnswers}`)
+    });
+});
+
