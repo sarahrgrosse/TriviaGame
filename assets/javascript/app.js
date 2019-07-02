009ce024954c121bca55d0a022b94d8f90d157ea
Sarah Rhianna Grosse <sarahrgrosse@Sarahs-MacBook-Air.local>
2019-07-02 17:01:12 -0400

    timer added plus other ways to work through array

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,23 +1,74 @@
 $(document).ready(function () {
-    $("#quiz").submit(function (event) {
-        event.preventDefault();
 
-        var $inputs = $('#quiz input[type=\'radio\']:checked');
+    // var questions = [{q:"is 1 greater than 0", c: true},{ q: "is 2 smaller than 1?", c: f}]
 
-        let correctAnswers = 0
 
-        $inputs.each(function () {
-            // if the value is true, they chose the correct answer
-            let answer = $(this).val();
-            if (answer === "true") {
-                correctAnswers = correctAnswers + 1
+    // for(var i =0; i< questions.length; i++){
+    //     $('<h1>').text(questions[i].q);
+    // }
+
+    var timer = 30;
+    var interval = setInterval(function () {
+        timer--;
+        $('#timer').text(timer)
+        subtmitAnswer();
+        if (timer === 0) {
+            clearInterval(interval)
+        }
+    }, 1000)
+
+
+    function subtmitAnswer () {
+        $("#quiz").submit(function (event) {
+            event.preventDefault();
+
+            var $inputs = $('#quiz input[type=\'radio\']:checked');
+
+            let correctAnswers = 0
+            //cookies.length = 4
+            //Array  index        0         1          2          3     
+            var cookies = ["cookie1", "cookie2", "cookie3", "cookie4"];
+            // console.log('Prince has eaten ' + cookies[0])
+            // console.log('Prince has eaten ' + cookies[1])
+            // console.log('Prince has eaten ' + cookies[2])
+            // console.log('Prince has eaten ' + cookies[3])
+
+            for (var i = 0; i < cookies.length; i++) {
+                console.log('Prince has eaten ' + cookies[i]);
+                //i=0
+                //i=1
+                //i=2
+                //i=3
             }
 
 
 
-        })
 
-        console.log(`you have this many correct: ${correctAnswers}`)
-    });
+            //for each 
+            //for loop
+            //for each 
+            $inputs.each(function () {
+                // if the value is true, they chose the correct answer
+                let answer = $(this).val();
+                if (answer === "true") {
+                    correctAnswers++;
+                }
+            })
+
+            clearInterval(interval)
+            $('#result').text(
+                `you have this many correct: ${correctAnswers}`
+
+            )
+            // console.log(`you have this many correct: ${correctAnswers}`)
+        });
+    }
 });
 
+
+
+
+
+// function stopTimer(){
+//     clearInterval(interval)
+// }
